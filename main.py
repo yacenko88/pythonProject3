96aa5c5fda9d33cdf5979913015961c568c63b4c
asd <yacenko88@gmail.com>
2022-05-13 22:24:02 +0300

    lesson #1

--- main.py
+++ main.py
@@ -5,9 +5,13 @@ def req_g():
 
     response = requests.get('https://scrapingclub.com/exercise/list_basic/?page=1')
     soup = BeautifulSoup(response.text, 'lxml')
-    data = soup.find('div', class_='col-lg-4 col-md-6 mb-4')
-    name = soup.find('h4', class_='card-title').text.replace('\n','')
-    print(name)
+    data = soup.find_all('div', class_='col-lg-4 col-md-6 mb-4')
+    for i in data:
+        name = i.find('h4', class_='card-title').text.replace('\n','')
+        price = i.find('h5').text
+        url_img = 'https://scrapingclub.com' + i.find('img', class_='card-img-top img-fluid').get('src')
+        print(name + '\n' + price + '\n' + url_img + '\n\n')
+    # print(data)
 
 def main():
     req_g()
